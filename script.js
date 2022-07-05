79a0a162048bfc8097853cf3710eb2ec1da5fe68
Prajapatiamankumar <91087352+Prajapatiamankumar@users.noreply.github.com>
2022-07-05 23:21:00 +0530

    html, css, and javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+function openForm(){
+    document.getElementById("openform").style.display="block";
+}
+
+function closeForm(){
+    document.getElementById("openform").style.display="none";
+}
+//admin login
+
+function login(){
+    document.getElementById("adminform").style.display="block";
+}
+//close login
+function closeLogin(){
+    document.getElementById("adminform").style.display="none"
+}
